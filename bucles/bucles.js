a8311d4a65d873a264ca4c325bc10b11ed21d202
alejomedina305-glitch <alejomedina305@gmail.com>
2026-06-25 19:02:47 -0300

    commit Condicionales y Bucles

---
+++ bucles/bucles.js
@@ -0,0 +1,6 @@
+let nombre = ["Ada", "Juan", "María"];
+let edad = [19, 25, 30];
+
+for (let i = 0; i < nombre.length; i++) {
+    console.log(nombre[i] + " tiene " + edad[i] + " años.");
+}
---
+++ condicionales/condicionales.js
@@ -0,0 +1,8 @@
+let nombre = "Ada";
+let edad = 19;
+
+if (edad >= 18) {
+    console.log(nombre + " es mayor de edad");
+} else {
+    console.log(nombre + " no es mayor de edad");
+}
